6436533d1c08871f2b956b1eccd507c6d5beb965
“SivonneWard” <sivonne@live.unc.edu>
2018-10-20 13:55:17 -0400

    starting git repo

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  console.log('scripts loaded');
+
+/*
+1) Build an HTML table using an AJAX call on the provided XML file (NEH_Grants2010s.xml).
+   The XML data shows all of the grants awarded by the National Endowment for the Humanities since 2008.
+2) The table should have four columns:
+    The Project Title, Year Awarded, Original Amount, and grant description (ToSupport)
+3) You will notice that the table is a bit messy; some of the grants have no descriptions, leaving large
+   blank spaces with just 'None.' Clean this up with conditional logic in your code.
+   If the grant has no description, do not include it in the table.
+*/
+
+
+});
