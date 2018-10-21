0f5b25c7d01ea1167acd40b1489716e550c92b07
“SivonneWard” <sivonne@live.unc.edu>
2018-10-21 14:28:30 -0400

    got table to show, using class exercises on AJAX, XML, and Exercise 5 as reference. needs css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,44 @@
 $(document).ready(function(){
   console.log('scripts loaded');
+//establish variables to retrieve from xml file.
+  var grants;
+  var html = '';
+  var url = 'NEH_Grants2010s.xml';
+  var projectTitle = '';
+  var yearAwarded = '';
+  var originalAmount = '';
+  var grantDescription = '';
+//call ajax
+  $.ajax({
+    type:'GET',
+    url: url,
+    data: grants,
+    dataType: 'xml',
+    async: true,
+    success: function(grants){
+      //set up column names
+      html += '<tr><th>The Project Title</th><th>Year Awarded</th><th>Original Amount</th><th>Grant Description (ToSupport)</th></tr>';
+      //get the data from xml file and build table with it
+    $(grants).find('Grant').each(function(){
+        projectTitle = $(this).find('ProjectTitle').text();
+        yearAwarded = $(this).find('YearAwarded').text();
+        originalAmount = $(this).find('OriginalAmount').text();
+        grantDescription = $(this).find('ToSupport').text();
+        //makes sure only table entries with descriptions are shown
+        if(grantDescription != 'None'){
+          html += '<td><b>' + projectTitle + '</b>';
+          html += '<td><b>' + yearAwarded + '</b>';
+          html += '<td><b>' + originalAmount + '</b>';
+          html += '<td><b>' + grantDescription + '</b> <tr> </tr>';
+          html += '</td>';
+        }
+  });
 
+  $("#results").append(html);
+
+  }
+  });
+});
 /*
 1) Build an HTML table using an AJAX call on the provided XML file (NEH_Grants2010s.xml).
    The XML data shows all of the grants awarded by the National Endowment for the Humanities since 2008.
@@ -10,6 +48,3 @@ $(document).ready(function(){
    blank spaces with just 'None.' Clean this up with conditional logic in your code.
    If the grant has no description, do not include it in the table.
 */
-
-
-});
